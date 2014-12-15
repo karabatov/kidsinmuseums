f1d3862bd587f73dfaf6959bd8c07b90c55ef3fb
Yuri Karabatov <yuri@yurikarabatov.com>
2014-12-15 16:04:17 +0300

    Initial Commit

---
+++ KidsInMuseums/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KidsInMuseums
+//
+//  Created by Yuri Karabatov on 15.12.14.
+//  Copyright (c) 2014 Golova Media. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
