d98ef3f5e5beaf440f363a33e099eaca7f4eb6eb
Yuri Karabatov <yuri@yurikarabatov.com>
2015-01-22 14:31:36 +0300

    Map tab icon.

--- KidsInMuseums/MapViewController.swift
+++ KidsInMuseums/MapViewController.swift
@@ -13,11 +13,11 @@ class MapViewController: UIViewController {
 
     override func viewDidLoad() {
         title = NSLocalizedString("Map", comment: "Map controller title")
-//        tabBarItem = UITabBarItem(title: title, image: UIImage(named: "icon-news"), tag: 0)
+        tabBarItem = UITabBarItem(title: title, image: UIImage(named: "icon-map"), tag: 0)
         navigationItem.backBarButtonItem = UIBarButtonItem(title: NSLocalizedString("Back", comment: "Navbar back button title"), style: .Plain, target: nil, action: nil)
         edgesForExtendedLayout = UIRectEdge.None
 
-        var camera = GMSCameraPosition.cameraWithLatitude(55.75, longitude: 37.61, zoom: 10)
+        var camera = GMSCameraPosition.cameraWithLatitude(55.75, longitude: 37.61, zoom: 12)
         var mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
         mapView.myLocationEnabled = true
         view = mapView
